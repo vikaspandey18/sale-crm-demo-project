8b2905a2cdfa0975540ee4696b03ad526e8c6640
vikas <vp2841144@gmail.com>
2026-03-16 06:01:12 +0530

    Adding the concatLatestFrom in collection-report,my-report,order-report

--- src/app/pages/reports/my-report/state/my-report.effects.ts
+++ src/app/pages/reports/my-report/state/my-report.effects.ts
@@ -7,16 +7,25 @@ import {
   fetchMyReportStartAction,
   fetchMyReportSuccessAction,
 } from "./my-report.actions";
+import { concatLatestFrom } from "@ngrx/operators";
+import { Store } from "@ngrx/store";
+import { AppState } from "../../../../store/app.state";
+import { getMyReportSelector } from "./my-report.selectors";
 
 @Injectable()
 export class MyReportEffect {
+  private store = inject(Store<AppState>);
   private actions$ = inject(Actions);
   private myReportService = inject(ReportService);
 
   getMyReport$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(fetchMyReportStartAction),
-      concatMap((action) => {
+      concatLatestFrom(() => this.store.select(getMyReportSelector)),
+      concatMap(([action, reports]) => {
+        if (reports.length) {
+          return of(fetchMyReportSuccessAction({ reports }));
+        }
         return this.myReportService.getMyReport().pipe(
           map((response) => {
             return fetchMyReportSuccessAction({ reports: response.data });
--- src/app/pages/reports/order-report/state/order-report.effects.ts
+++ src/app/pages/reports/order-report/state/order-report.effects.ts
@@ -7,16 +7,25 @@ import {
   fetchOrderReportSuccessAction,
 } from "./order-report.actions";
 import { catchError, concatMap, map, of } from "rxjs";
+import { Store } from "@ngrx/store";
+import { AppState } from "../../../../store/app.state";
+import { concatLatestFrom } from "@ngrx/operators";
+import { getOrderReportSelector } from "./order-report.selectors";
 
 @Injectable()
 export class OrderReportEffect {
+  private store = inject(Store<AppState>);
   private actions$ = inject(Actions);
   private orderReportService = inject(OrderReportService);
 
   orderReport$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(fetchOrderReportStartAction),
-      concatMap((action) => {
+      concatLatestFrom(() => this.store.select(getOrderReportSelector)),
+      concatMap(([action, orders]) => {
+        if (orders.length > 0) {
+          return of(fetchOrderReportSuccessAction({ orders }));
+        }
         return this.orderReportService.getOrderReport().pipe(
           map((response) => {
             return fetchOrderReportSuccessAction({ orders: response.data });
