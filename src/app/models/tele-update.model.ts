3c71d604ed2d338a8919308601dc7862aa540977
vikas <vp2841144@gmail.com>
2026-03-19 19:42:33 +0530

    Added telecaller model request

---
+++ src/app/models/tele-update.model.ts
@@ -0,0 +1,15 @@
+export interface TeleRequest {
+  activity?: string;
+  amount?: string;
+  comment?: string;
+  customerId?: string;
+  followupDate?: string;
+  followupResponse?: string;
+  leadGrade?: string;
+  newCustomerGroup?: string;
+  partyType?: string;
+  paymentDone?: string;
+  paymentStatus?: string;
+  products?: string[];
+  purchaser?: string;
+}
