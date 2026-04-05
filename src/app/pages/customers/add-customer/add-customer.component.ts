08e8efca4bf1d10c5ea60e9bb212ff2adc180f4f
vikas <vp2841144@gmail.com>
2026-04-05 16:08:54 +0530

    Added the blank form for the add customer

--- src/app/pages/customers/add-customer/add-customer.component.ts
+++ src/app/pages/customers/add-customer/add-customer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PageBreadcrumbComponent } from "../../../shared/components/common/page-breadcrumb/page-breadcrumb.component";
 
 @Component({
   selector: 'app-add-customer',
-  imports: [],
+  imports: [PageBreadcrumbComponent],
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css',
 })
