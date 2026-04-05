03bc08c9bf3449ee44a5f354ae82c7e1365d8e0a
vikas <vp2841144@gmail.com>
2026-04-05 15:37:31 +0530

    Added two component in customer whith add customer and delete customer

---
+++ src/app/pages/customers/delete-customer/delete-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-customer',
+  imports: [],
+  templateUrl: './delete-customer.component.html',
+  styleUrl: './delete-customer.component.css',
+})
+export class DeleteCustomerComponent {
+
+}
